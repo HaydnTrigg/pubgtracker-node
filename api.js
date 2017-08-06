45f3b244096df5dc4922be75a115c431eeef0a4a
Haydn Trigg <me@haydntrigg.com>
2017-08-06 20:41:33 +0930

    Restored back to working API

--- api.js
+++ api.js
@@ -15,44 +15,26 @@ class PubgTracker {
    * Retrieve the account of a player via their StreamID
    * @param {string} id The 64bit SteamID as a string
    */
-  getPlayerByStreamID64(id, priority) {
+  getPlayerByStreamID64(id) {
     const options = {
-      uri: `https://pubgtracker.com/api/search?steamId=${id}`,
+      url: `https://pubgtracker.com/api/search?steamId=${id}`,
       headers: {
         'TRN-Api-Key': this.api_key
       },
       json: true
     };
-
-    return new Promise((resolve, reject) => {
-      this.limiter.submitPriority(priority || 5, async (options) => {
-        try {
-          resolve(await request.get(options))
-        } catch(err) {
-          reject(err)
-        }
-      }, options)
-    })
+    return request.get(options)
   }
 
-  getStatsByNickname(nickname, priority) {
+  getStatsByNickname(nickname) {
     const options = {
-      uri: `https://pubgtracker.com/api/profile/pc/${nickname}`,
+      url: `https://pubgtracker.com/api/profile/pc/${nickname}`,
       headers: {
         'TRN-Api-Key': this.api_key
       },
       json: true
     };
-
-    return new Promise((resolve, reject) => {
-      this.limiter.submitPriority(priority || 5, async (options) => {
-        try {
-          resolve(await request.get(options))
-        } catch(err) {
-          reject(err)
-        }
-      }, options)
-    })
+    return request.get(options)
   }
 }
 
