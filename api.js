7f557b3b364c84d10904a329d07dfb5331b4e911
Haydn Trigg <me@haydntrigg.com>
2017-08-07 16:48:37 +0930

    Added Correct Rate Limit

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ class PubgTracker {
 
     // Limit the API to ~1/sec as per the request of https://pubgtracker.com/site-api
     //this.limiter = new Bottleneck(1, 1000);
-    this.limiter = new Bottleneck(10, 10000);
+    this.limiter = new Bottleneck(1, 1000);
   }
 
   /**
@@ -16,26 +16,55 @@ class PubgTracker {
    * @param {string} id The 64bit SteamID as a string
    */
   getPlayerByStreamID64(id) {
-    const options = {
-      url: `https://pubgtracker.com/api/search?steamId=${id}`,
-      headers: {
-        'TRN-Api-Key': this.api_key
-      },
-      json: true
-    };
-    return request.get(options)
+
+    return new Promise((resolve, reject) => {
+
+      const options = {
+        url: `https://pubgtracker.com/api/search?steamId=${id}`,
+        headers: {
+          'TRN-Api-Key': this.api_key
+        },
+        json: true
+      };
+
+      return new Promise((resolve, reject) => {
+        this.limiter.submit((callback) => {
+          request.get(options, callback)
+        }, (err, response, body) => {
+          if (err) reject(err)
+          else resolve(body)
+        })
+      })
+
+    })
+
   }
 
-  getStatsByNickname(nickname) {
-    const options = {
-      url: `https://pubgtracker.com/api/profile/pc/${nickname}`,
-      headers: {
-        'TRN-Api-Key': this.api_key
-      },
-      json: true
-    };
-    return request.get(options)
+  getStatsByNickname(id) {
+
+    return new Promise((resolve, reject) => {
+
+      const options = {
+        url: `https://pubgtracker.com/api/profile/pc/${nickname}`,
+        headers: {
+          'TRN-Api-Key': this.api_key
+        },
+        json: true
+      };
+
+      return new Promise((resolve, reject) => {
+        this.limiter.submit((callback) => {
+          request.get(options, callback)
+        }, (err, response, body) => {
+          if (err) reject(err)
+          else resolve(body)
+        })
+      })
+
+    })
+
   }
+
 }
 
 module.exports = PubgTracker;
