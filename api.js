a1931ef51cd4db9802bca239b958d28bd33101c5
Haydn Trigg <me@haydntrigg.com>
2017-12-19 01:38:43 +1030

    PubgTrackerError

--- api.js
+++ api.js
@@ -1,14 +1,13 @@
 const Bottleneck = require("bottleneck");
 const request = require("request");
 
-
 class PubgTracker {
   constructor(api_key) {
     this.api_key = api_key
 
     // Limit the API to ~1/sec as per the request of https://pubgtracker.com/site-api
     //this.limiter = new Bottleneck(1, 1000);
-    this.limiter = new Bottleneck(1, 1000);
+    this.limiter = new Bottleneck(1, arguments[1] || 2000);
   }
 
   /**
@@ -17,54 +16,65 @@ class PubgTracker {
    */
   getPlayerByStreamID64(id) {
 
+    const options = {
+      url: `https://api.pubgtracker.com/v2/search/steam?steamId=${id}`,
+      headers: {
+        'TRN-Api-Key': this.api_key
+      },
+      json: true
+    };
+
     return new Promise((resolve, reject) => {
-
-      const options = {
-        url: `https://api.pubgtracker.com/v2/search/steam?steamId=${id}`,
-        headers: {
-          'TRN-Api-Key': this.api_key
-        },
-        json: true
-      };
-
-      return new Promise((resolve, reject) => {
-        this.limiter.submit((callback) => {
-          request.get(options, callback)
-        }, (err, response, body) => {
-          if (err) reject(err)
-          else resolve(body)
-        })
+      this.limiter.submit((callback) => {
+        request.get(options, callback)
+      }, (err, response, body) => {
+        if (err) reject(err)
+        else if (body.error) reject(new PubgTrackerError(body))
+        else resolve(body)
       })
-
     })
 
   }
 
-  getStatsByNickname(id) {
+  getStatsByNickname(nickname) {
+
+    const options = {
+      url: `https://api.pubgtracker.com/v2/profile/pc/${nickname}`,
+      headers: {
+        'TRN-Api-Key': this.api_key
+      },
+      json: true
+    };
 
     return new Promise((resolve, reject) => {
-
-      const options = {
-        url: `https://api.pubgtracker.com/v2/profile/pc/${nickname}`,
-        headers: {
-          'TRN-Api-Key': this.api_key
-        },
-        json: true
-      };
-
-      return new Promise((resolve, reject) => {
-        this.limiter.submit((callback) => {
-          request.get(options, callback)
-        }, (err, response, body) => {
-          if (err) reject(err)
-          else resolve(body)
-        })
+      this.limiter.submit((callback) => {
+        request.get(options, callback)
+      }, (err, response, body) => {
+        if (err) reject(err)
+        else if (body.error) reject(new PubgTrackerError(body))
+        else resolve(body)
       })
-
     })
 
   }
 
 }
 
+class PubgTrackerError extends Error {
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this, PubgTrackerError)
+
+    const [arg1] = args
+    if (typeof arg1 === 'string') this.message = arg1;
+    else if (typeof arg1 === 'object') {
+      this.message = arg1.message;
+      this.code = arg1.error;
+    }
+
+
+  }
+}
+
 module.exports = PubgTracker;
+module.exports.PubgTrackerError = PubgTrackerError;
