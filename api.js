d6da8eb6af59bfe5e3a37f89ebd057b94cf1a19a
Haydn Trigg <me@haydntrigg.com>
2017-12-19 17:19:44 +1030

    API Rate Limit Error

--- api.js
+++ api.js
@@ -7,15 +7,19 @@ class PubgTracker {
 
     // Limit the API to ~1/sec as per the request of https://pubgtracker.com/site-api
     //this.limiter = new Bottleneck(1, 1000);
-    this.limiter = new Bottleneck(1, arguments[1] || 2000);
+
+    const args = arguments[1] || {};
+    this.args = args;
+    const rate_limit_time = Math.max((args.rate_limit_time || 2000), 2000);
+    this.limiter = new Bottleneck(1, rate_limit_time + 100);
   }
 
   /**
    * Retrieve the account of a player via their StreamID
    * @param {string} id The 64bit SteamID as a string
    */
-  getPlayerByStreamID64(id) {
-	if(!id || typeof id !== 'string') throw new Error('id is invalid')
+  async getPlayerBySteamID64(id, retry) {
+    if (!id || typeof id !== 'string') throw new Error('id is invalid')
 
     const options = {
       url: `https://api.pubgtracker.com/v2/search/steam?steamId=${id}`,
@@ -25,7 +29,7 @@ class PubgTracker {
       json: true
     };
 
-    return new Promise((resolve, reject) => {
+    const result = await new Promise((resolve, reject) => {
       this.limiter.submit((callback) => {
         request.get(options, callback)
       }, (err, response, body) => {
@@ -35,10 +39,17 @@ class PubgTracker {
       })
     })
 
+    if (result.message == "API rate limit exceeded") {
+      const maximum_retry = (this.args.maximum_retry || 5);
+      if (retry >= maximum_retry) throw new PubgTrackerError(result);
+      retry = (retry || 0) + 1;
+      return await this.getPlayerBySteamID64(id, retry);
+    } else return result;
+
   }
 
-  getStatsByNickname(nickname) {
-	if(!nickname || typeof nickname !== 'string') throw new Error('nickname is invalid')
+  async getStatsByNickname(nickname, retry) {
+    if (!nickname || typeof nickname !== 'string') throw new Error('nickname is invalid')
     const options = {
       url: `https://api.pubgtracker.com/v2/profile/pc/${nickname}`,
       headers: {
@@ -47,7 +58,7 @@ class PubgTracker {
       json: true
     };
 
-    return new Promise((resolve, reject) => {
+    const result = await new Promise((resolve, reject) => {
       this.limiter.submit((callback) => {
         request.get(options, callback)
       }, (err, response, body) => {
@@ -56,6 +67,13 @@ class PubgTracker {
         else resolve(body)
       })
     })
+
+    if (result.message == "API rate limit exceeded") {
+      const maximum_retry = (this.args.maximum_retry || 5);
+      if (retry >= maximum_retry) throw new PubgTrackerError(result);
+      retry = (retry || 0) + 1;
+      return await this.getStatsByNickname(nickname, retry);
+    } else return result;
 
   }
 
